b9db4947303ae4582eb880aa0ac5acf0fd5e1ddf
TechWithTy <tyrique1.daniel@gmail.com>
2025-05-04 15:50:52 -0600

    Cleanup: remove obsolete cache metrics and update docs

--- metrics.py
+++ metrics.py
@@ -32,8 +32,70 @@ CELERY_TASK_LATENCY = Histogram(
     ['task_name']
 )
 
+# Celery cache metrics
+CELERY_CACHE_HITS = Counter(
+    'celery_cache_hits_total',
+    'Number of cache hits inside Celery tasks',
+    ['task_name']
+)
+CELERY_CACHE_MISSES = Counter(
+    'celery_cache_misses_total',
+    'Number of cache misses inside Celery tasks',
+    ['task_name']
+)
+CELERY_CACHE_SETS = Counter(
+    'celery_cache_sets_total',
+    'Number of cache set operations inside Celery tasks',
+    ['task_name']
+)
+CELERY_CACHE_DELETES = Counter(
+    'celery_cache_deletes_total',
+    'Number of cache delete operations inside Celery tasks',
+    ['task_name']
+)
+
 # System metrics
 SYSTEM_CPU_USAGE = Gauge(
     'system_cpu_usage_percent',
     'System CPU usage percentage'
+)
+
+# Pulsar cache metrics
+PULSAR_CACHE_HITS = Counter(
+    'pulsar_cache_hits_total',
+    'Number of cache hits for Pulsar operations'
+)
+PULSAR_CACHE_MISSES = Counter(
+    'pulsar_cache_misses_total',
+    'Number of cache misses for Pulsar operations'
+)
+PULSAR_CACHE_SETS = Counter(
+    'pulsar_cache_sets_total',
+    'Number of cache sets for Pulsar operations'
+)
+PULSAR_CACHE_DELETES = Counter(
+    'pulsar_cache_deletes_total',
+    'Number of cache deletes for Pulsar operations'
+)
+
+# VALKEY cache metrics
+VALKEY_CACHE_HITS = Counter(
+    'valkey_cache_hits_total',
+    'Number of cache hits for VALKEY operations'
+)
+VALKEY_CACHE_MISSES = Counter(
+    'valkey_cache_misses_total',
+    'Number of cache misses for VALKEY operations'
+)
+VALKEY_CACHE_SETS = Counter(
+    'valkey_cache_sets_total',
+    'Number of cache sets for VALKEY operations'
+)
+VALKEY_CACHE_DELETES = Counter(
+    'valkey_cache_deletes_total',
+    'Number of cache deletes for VALKEY operations'
+)
+VALKEY_CACHE_ERRORS = Counter(
+    'valkey_cache_errors_total',
+    'Number of errors during VALKEY cache operations'
 )
